ac7ffd1c86e3ba11735108bf4ce7bd7b6a6387bc
asyifallya <nabilaasyifa47@gmail.com>
2025-12-23 08:30:44 +0700

    Create checkout.php

---
+++ checkout.php
@@ -0,0 +1,18 @@
+<?php
+include 'config.php';
+unset($_SESSION['keranjang']);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Checkout</title>
+</head>
+<body>
+
+<h2>✅ Transaksi Berhasil</h2>
+<p>Terima kasih telah berbelanja di Toko Buah 🍎</p>
+<a href="index.php">Kembali ke Toko</a>
+
+</body>
+</html>
